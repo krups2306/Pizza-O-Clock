301cbc5657c767db40cf407ffa7587604d2ea744
Krupa Bhavsar <krups4444@gmail.com>
2022-05-13 18:45:49 +0530

    db setup

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express  = require("express");
 const db = require("./db")
 const app = express();
+const Pizza = require('./models/pizzaModel')
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Server working");
+});
+
+app.get("/getpizzas", (req, res)=>{
+    Pizza.find({}, (err, docs)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send(docs)
+        }
+    })
+
 });
 
 const port = process.env.PORT || 5000;
